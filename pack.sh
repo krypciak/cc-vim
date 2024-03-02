0d6e3c7652f11c20327aad952c5f2946a69f21f6
krypek <krypek@tuta.io>
2024-03-02 11:23:46 +0100

    Improve pack.sh

--- pack.sh
+++ pack.sh
@@ -4,6 +4,14 @@ NAME="${BASE_NAME}-$(jq '.version' ccmod.json | sed 's/^"//;s/"$//').ccmod"
 rm -rf "$BASE_NAME"*
 npm install
 npm run build
-zip -r "$NAME" ./ -x "*.ccmod" "*.zip" "node_modules/*" ".git*" "*.ts" "*.md" \
-    "tsconfig.json" "*.sh" "package-lock.json" "pack/*" "bundle.zip" ".prettierrc.json" \
-    "*.kra" "*.kra~" "*.png~" "src/*"
+mkdir -p pack
+cp -r icon LICENSE plugin.js ./pack
+cd ./pack
+for file in $(find . -iname '*.json'); do
+    jq '.' ../$file -c > $file
+done
+cp ../ccmod.json .
+rm -rf icon/icon.kra icon/icon.png~
+zip -r "../$NAME" .
+cd ..
+rm -rf pack
